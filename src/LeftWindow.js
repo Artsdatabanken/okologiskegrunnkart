611dbdfd6f66415d35213df0924507c656dece13
Helene Engeness Mørk <helemork@gmail.com>
2020-01-15 17:29:39 +0100

    et forsøk på å separere markørinnhold fra kartlag

---
+++ src/LeftWindow.js
@@ -0,0 +1,9 @@
+import ForvaltningsKartlag from "Forvaltningsportalen/ForvaltningsKartlag/ForvaltningsKartlag";
+import React from "react";
+import FeatureInfo from "./FeatureInfo";
+
+const LeftWindow = props => {
+  return <FeatureInfo {...props}></FeatureInfo>;
+};
+
+export default LeftWindow;
--- src/RightWindow.js
+++ src/RightWindow.js
@@ -1,26 +1,8 @@
 import ForvaltningsKartlag from "Forvaltningsportalen/ForvaltningsKartlag/ForvaltningsKartlag";
-import React, { useState } from "react";
-import { AppBar, Tabs, Typography, Tab } from "@material-ui/core";
+import React from "react";
 import FeatureInfo from "./FeatureInfo";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <>{children}</>}
-    </Typography>
-  );
-}
 const RightWindow = props => {
-  const [tab, setTab] = useState(0);
   return (
     <div
       style={{
@@ -34,26 +16,15 @@ const RightWindow = props => {
         zIndex: -1
       }}
     >
-      <AppBar position="static">
-        <Tabs
-          variant="fullWidth"
-          value={tab}
-          onChange={(e, v) => setTab(v)}
-          aria-label="simple tabs example"
-        >
-          <Tab label="Kartlag" />
-          <Tab label="Markør" />
-        </Tabs>
-      </AppBar>
-      <TabPanel
-        value={tab}
-        index={0}
+      <h3>Kartlag</h3>
+      <div
         style={{
           position: "absolute",
           top: 56,
           bottom: 0,
           overflowY: "auto",
-          paddingBottom: 48
+          paddingBottom: 48,
+          width: "100%"
         }}
       >
         <ForvaltningsKartlag
@@ -67,20 +38,7 @@ const RightWindow = props => {
           history={props.history}
           onUpdateLayerProp={props.onUpdateLayerProp}
         />
-      </TabPanel>
-      <TabPanel
-        value={tab}
-        index={1}
-        style={{
-          position: "absolute",
-          top: 56,
-          bottom: 0,
-          overflowY: "auto",
-          paddingBottom: 48
-        }}
-      >
-        <FeatureInfo {...props}></FeatureInfo>
-      </TabPanel>
+      </div>
     </div>
   );
 };
