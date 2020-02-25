ba52462f63e9b7b136796724d6240f103c62e37a
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-02-25 12:36:26 +0100

    Remove node_modules

--- lastejobb/node_modules/wms-capabilities/src/utils/isdef.js
+++
@@ -1,8 +0,0 @@
-/**
- * Returns true if the specified value is not undefined.
- *
- * @param {?} val Variable to test.
- * @return {Boolean} Whether variable is defined.
- */
-export default (val) => val !== void 0;
-
