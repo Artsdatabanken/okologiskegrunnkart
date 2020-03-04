6bb5f3b112fd250478777edf4f08a34f8085c6b5
Helene Engeness Mørk <helemork@gmail.com>
2020-03-04 12:35:27 +0100

    boksene er mye lykkeligere (knapper kræsjer ikke med scrolling

--- src/Forvaltningsportalen/FeatureInfo/index.js
+++ src/Forvaltningsportalen/FeatureInfo/index.js
@@ -1,8 +1,7 @@
-import LocationSearching from "@material-ui/icons/LocationSearching";
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
-import { Close } from "@material-ui/icons";
+import { Close, Layers } from "@material-ui/icons";
 import GeneriskElement from "./GeneriskElement";
 import "style/infobox.css";
 
@@ -13,54 +12,33 @@ const FeatureInfo = ({
   handleExtensiveInfo,
   lat,
   lng,
-  resultat,
   layersresultat,
   sted,
   kommune
 }) => {
   if (!showExtensiveInfo) return null;
   if (!lat) return null;
-  const kommunestr =
-    resultat &&
-    resultat.kommune &&
-    resultat.kommune.kommune &&
-    resultat.kommune.kommune.tittel.nb +
-      " kommune i " +
-      resultat.kommune.fylke.tittel.nb;
   return (
     <div className="infobox_container">
-      <div className="left_window">
-        <div className="left_window_scrollable">
-          <button
-            className="close_button"
-            onClick={e => {
-              handleExtensiveInfo(false);
-            }}
-          >
-            <Close />
-          </button>
+      <div className="all_layer_results">
+        <button
+          className="close_button"
+          onClick={e => {
+            handleExtensiveInfo(false);
+          }}
+        >
+          <Close />
+        </button>
+
+        <ListItem>
+          <ListItemIcon>
+            <Layers />
+          </ListItemIcon>
+          <ListItemText primary={"Resultat fra alle kartlag"} />
+        </ListItem>
+
+        <div className="all_layer_results_scrollable">
           <List>
-            {lat && (
-              <ListItem button>
-                <ListItemIcon>
-                  <LocationSearching />
-                </ListItemIcon>
-                <ListItemText
-                  primary={sted && sted.navn}
-                  secondary={kommunestr}
-                />
-              </ListItem>
-            )}
-            {resultat !== undefined &&
-              Object.keys(resultat).map(key => {
-                return (
-                  <GeneriskElement
-                    kartlag={kartlag}
-                    resultat={resultat[key]}
-                    element={key}
-                  />
-                );
-              })}
             {layersresultat !== undefined &&
               Object.keys(layersresultat).map(key => {
                 return (
