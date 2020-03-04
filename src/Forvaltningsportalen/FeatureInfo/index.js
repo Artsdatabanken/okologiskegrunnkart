baae6dfea71218f64bc7331d320055ae7d06ad69
Helene Engeness Mørk <helemork@gmail.com>
2020-03-04 12:22:24 +0100

    infoboksene og søk i alle lag boksene bor nå pent sammen

--- src/Forvaltningsportalen/FeatureInfo/index.js
+++ src/Forvaltningsportalen/FeatureInfo/index.js
@@ -4,6 +4,7 @@ import { withRouter } from "react-router-dom";
 import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import { Close } from "@material-ui/icons";
 import GeneriskElement from "./GeneriskElement";
+import "style/infobox.css";
 
 const FeatureInfo = ({
   showExtensiveInfo,
@@ -27,50 +28,52 @@ const FeatureInfo = ({
       " kommune i " +
       resultat.kommune.fylke.tittel.nb;
   return (
-    <div className="left_window">
-      <div className="left_window_scrollable">
-        <button
-          className="close_button"
-          onClick={e => {
-            handleExtensiveInfo(false);
-          }}
-        >
-          <Close />
-        </button>
-        <List>
-          {lat && (
-            <ListItem button>
-              <ListItemIcon>
-                <LocationSearching />
-              </ListItemIcon>
-              <ListItemText
-                primary={sted && sted.navn}
-                secondary={kommunestr}
-              />
-            </ListItem>
-          )}
-          {resultat !== undefined &&
-            Object.keys(resultat).map(key => {
-              return (
-                <GeneriskElement
-                  kartlag={kartlag}
-                  resultat={resultat[key]}
-                  element={key}
+    <div className="infobox_container">
+      <div className="left_window">
+        <div className="left_window_scrollable">
+          <button
+            className="close_button"
+            onClick={e => {
+              handleExtensiveInfo(false);
+            }}
+          >
+            <Close />
+          </button>
+          <List>
+            {lat && (
+              <ListItem button>
+                <ListItemIcon>
+                  <LocationSearching />
+                </ListItemIcon>
+                <ListItemText
+                  primary={sted && sted.navn}
+                  secondary={kommunestr}
                 />
-              );
-            })}
-          {layersresultat !== undefined &&
-            Object.keys(layersresultat).map(key => {
-              return (
-                <GeneriskElement
-                  key={key}
-                  kartlag={kartlag}
-                  resultat={layersresultat[key]}
-                  element={key}
-                />
-              );
-            })}
-        </List>
+              </ListItem>
+            )}
+            {resultat !== undefined &&
+              Object.keys(resultat).map(key => {
+                return (
+                  <GeneriskElement
+                    kartlag={kartlag}
+                    resultat={resultat[key]}
+                    element={key}
+                  />
+                );
+              })}
+            {layersresultat !== undefined &&
+              Object.keys(layersresultat).map(key => {
+                return (
+                  <GeneriskElement
+                    key={key}
+                    kartlag={kartlag}
+                    resultat={layersresultat[key]}
+                    element={key}
+                  />
+                );
+              })}
+          </List>
+        </div>
       </div>
     </div>
   );
