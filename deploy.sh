7b51475345583497f3fbdf74165c09749c6ad30e
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-03-05 07:12:22 +0100

    Deployment script #1

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,7 @@
 #These lines should also be added in the "script block" of travis.yml.
 #- export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 #- echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
-#echo $BRANCH
 BRANCH=$1
-#echo $BRANCH
-#echo $TRAVIS_REPO_SLUG
-#echo $TRAVIS_PULL_REQUEST
-github_message=https://nin.artsdatabanken.no/$BRANCH
 echo "Making archive..."
 tar --directory=build -zcf $BRANCH.tar.gz .
 echo "Deploying..."
@@ -22,9 +17,3 @@ if [ "${BRANCH}" == "master" ]
  else
   echo "This branch will not be deployed, since it's not the master branch."
 fi
-#Posting message to pull request in GitHub
-#if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
-#then
-#echo "Posting to github ..."
-#curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"${github_message}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-#fi
