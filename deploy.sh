e3a0aff757acca13a5645112d5192fd839f0b65b
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2020-03-05 10:04:50 +0100

    Abort on error (#59)

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 #These lines should also be added in the "script block" of travis.yml.
 #- export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 #- echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+set -e
 BRANCH=$1
 FILENAME=forvaltningsportal_$BRANCH.tar.gz
 echo "Making archive..."
@@ -14,7 +15,7 @@ if [ "${BRANCH}" == "master" ]
   #Posting to slack to trigger deployment
   curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
 
-  sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $BRANCH.tar.gz $scp_user@$prod_dest
+  sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $FILENAME $scp_user@$prod_dest
  else
   echo "This branch will not be deployed, since it's not the master branch."
 fi
