a285f0f6fc262cd45d467d7a240605881895f7a2
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-03-07 23:50:09 +0100

    fun fun

--- src/Funksjoner/språk.js
+++ src/Funksjoner/språk.js
@@ -1,18 +1,18 @@
-import {exportableSpraak} from "App";
+import { exportableSpraak } from "../App";
 
 export default function språk(meta) {
-  let spraak = exportableSpraak.state.spraak;  
+  let spraak = exportableSpraak.state.spraak;
   if (!meta) return "";
   if (typeof meta === "string") return meta;
-  const keys = Object.keys(meta);  
+  const keys = Object.keys(meta);
   if (!keys || keys.length === 0) return meta;
   if (keys.length === 1) return meta[keys[0]];
-  if(meta[spraak] && meta[spraak] !== undefined){
+  if (meta[spraak] && meta[spraak] !== undefined) {
     return meta[spraak];
-  }else if(meta.nb){
+  } else if (meta.nb) {
     return `${meta.nb}`;
   } else if (meta.nb === "undefined" && meta.sn !== undefined) {
     return `${meta.sn}`;
-  }   
+  }
   return meta[keys[0]];
 }
