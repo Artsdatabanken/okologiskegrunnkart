206fcfb465a246101d044c385fc7f3446548d9f6
Helene Engeness Mørk <helemork@gmail.com>
2020-03-12 15:28:01 +0100

    remigrerte etter db-tømming, og fikset encoding

--- django/forvaltningsportal/models.py
+++ django/forvaltningsportal/models.py
@@ -33,7 +33,8 @@ class Type(models.Model):
 class Kartlag(models.Model):
     tittel = models.CharField(max_length=200)
     kode = models.CharField(max_length=50,blank=True)
-    karturl = models.CharField(max_length=500,blank=True)
+    wmsurl = models.CharField(max_length=500,blank=True)
+    wmslayer = models.CharField(max_length=100,blank=True)
     faktaark = models.CharField(max_length=500,blank=True)
     klikkurl = models.CharField(max_length=500,blank=True)
     type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
@@ -53,18 +54,27 @@ def createJSON(sender, instance, **kwargs):
         dict[kartlag.tittel] = {
             'dataeier': kartlag.dataeier.tittel,
             'tittel': kartlag.tittel
-
         }
         if kartlag.type:
             dict[kartlag.tittel]['type'] = kartlag.type.tittel
+        if kartlag.tema:
+            dict[kartlag.tittel]['tema'] = kartlag.tema.tittel
         if kartlag.faktaark:
-            dict[kartlag.faktaark]['faktaark'] = kartlag.faktaark
+            dict[kartlag.tittel]['faktaark'] = kartlag.faktaark
+        if kartlag.wmsurl:
+            dict[kartlag.tittel]['wmsurl'] = kartlag.wmsurl
+        if kartlag.wmslayer:
+            dict[kartlag.tittel]['wmslayer'] = kartlag.wmslayer
         if kartlag.kode:
-            dict[kartlag.kode]['kode'] = kartlag.kode    
+            dict[kartlag.tittel]['kode'] = kartlag.kode
+        if kartlag.klikkurl:
+            dict[kartlag.tittel]['klikkurl'] = kartlag.klikkurl
+
+        # og så legg til nye felt, som tags osv
 
 
     print(json.dumps(dict,indent=4,sort_keys=True))
-    with open("kartlag_ny.json","w") as file:
-        json.dump(dict,file,indent=4,sort_keys=True)
+    with open("kartlag_ny.json","w", encoding='utf8') as file:
+        json.dump(dict,file,indent=4,sort_keys=True, ensure_ascii=False)
 
     print("************FINISHED doing the thing************")
