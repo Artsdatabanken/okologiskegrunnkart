c91a5956e4f4d7245d9e53db6cfd2ab49ba56135
Helene Engeness Mørk <helemork@gmail.com>
2020-03-12 13:14:30 +0100

    progress

--- django/forvaltningsportal/models.py
+++ django/forvaltningsportal/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-#from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 # Antar nå at alle url'er er lange, og at vi får slått sammen alle url-variablene til en variabel.
 # ignorerer for øyeblikket de som har felter som skiller seg ut fra resten
@@ -17,8 +19,8 @@ class Tag(models.Model):
 
 class Dataeier(models.Model):
     tittel = models.CharField(max_length=200)
-    logourl = models.CharField(max_length=500)
-    url = models.CharField(max_length=500)
+    logourl = models.CharField(max_length=500,blank=True)
+    url = models.CharField(max_length=500,blank=True)
     def __str__(self):
         return self.tittel
 
@@ -29,13 +31,18 @@ class Type(models.Model):
 
 class Kartlag(models.Model):
     tittel = models.CharField(max_length=200)
-    kode = models.CharField(max_length=50)
-    karturl = models.CharField(max_length=500)
-    faktaark = models.CharField(max_length=500)
-    klikkurl = models.CharField(max_length=500)
+    kode = models.CharField(max_length=50,blank=True)
+    karturl = models.CharField(max_length=500,blank=True)
+    faktaark = models.CharField(max_length=500,blank=True)
+    klikkurl = models.CharField(max_length=500,blank=True)
     type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
     dataeier = models.ForeignKey(Dataeier, on_delete=models.CASCADE)
     tema = models.ForeignKey(Tema, on_delete=models.SET_NULL, null=True)
-    tag = models.ManyToManyField(Tag)
+    tag = models.ManyToManyField(Tag, blank=True)
     def __str__(self):
        return self.tittel
+
+
+@receiver(post_save, sender=Kartlag)
+def save_profile(sender, instance, **kwargs):
+    print("************Doing the thing************")
