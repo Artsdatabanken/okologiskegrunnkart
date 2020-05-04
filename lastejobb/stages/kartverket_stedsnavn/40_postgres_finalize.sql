acbcd7cb91bd2a24a2c68559012122849e2bab79
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-05-04 10:05:14 +0200

    Prettify

--- lastejobb/stages/kartverket_stedsnavn/40_postgres_finalize.sql
+++ lastejobb/stages/kartverket_stedsnavn/40_postgres_finalize.sql
@@ -16,26 +16,24 @@ ALTER TABLE stedsnavn CLUSTER ON stedsnavn_geom_idx;
 
 
 -- REST API function 
--- http://localhost:3000/rpc/stedsnavn?lat=63&lng=10.5
+-- http://localhost:3000/rpc/stedsnavn?lat=63&lng=10.5&zoom=6
 
 CREATE OR REPLACE FUNCTION stedsnavn_zoom_til_minpri(zoom numeric)
 RETURNS character(10) AS $$
-       SELECT CASE 
-WHEN zoom<3 THEN 'viktighetN'
-WHEN zoom<4.8 THEN 'viktighetM'
-WHEN zoom<7 THEN 'viktighetL'
-WHEN zoom<8 THEN 'viktighetK'
-WHEN zoom<9 THEN 'viktighetJ'
-WHEN zoom<10 THEN 'viktighetI'
-WHEN zoom<11 THEN 'viktighetH'
-WHEN zoom<12 THEN 'viktighetG'
-WHEN zoom<13 THEN 'viktighetF'
-WHEN zoom<14 THEN 'viktighetE'
-WHEN zoom<15.5 THEN 'viktighetD'
---WHEN zoom<17 THEN 'viktighetC'
---WHEN zoom<18.5 THEN 'viktighetB'
-            ELSE 'viktighetC'
-       END;
+	SELECT CASE 
+		WHEN zoom<3 THEN 'viktighetN'
+		WHEN zoom<4.8 THEN 'viktighetM'
+		WHEN zoom<7 THEN 'viktighetL'
+		WHEN zoom<8 THEN 'viktighetK'
+		WHEN zoom<9 THEN 'viktighetJ'
+		WHEN zoom<10 THEN 'viktighetI'
+		WHEN zoom<11 THEN 'viktighetH'
+		WHEN zoom<12 THEN 'viktighetG'
+		WHEN zoom<13 THEN 'viktighetF'
+		WHEN zoom<14 THEN 'viktighetE'
+		WHEN zoom<15.5 THEN 'viktighetD'
+		ELSE 'viktighetC'
+    END;
 $$ LANGUAGE SQL;
 
 --       SELECT * FROM stedsnavn_zoom_til_minpri(zoom+3)
@@ -43,20 +41,20 @@ $$ LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION stedsnavn_zoom_til_maxpri(zoom numeric)
 RETURNS character(10) AS $$
-       SELECT CASE 
-WHEN zoom<3 THEN 'viktighetL'
-WHEN zoom<4.8 THEN 'viktighetK'
-WHEN zoom<7 THEN 'viktighetJ'
-WHEN zoom<8 THEN 'viktighetI'
-WHEN zoom<9 THEN 'viktighetH'
-WHEN zoom<10 THEN 'viktighetG'
-WHEN zoom<11 THEN 'viktighetF'
-WHEN zoom<12 THEN 'viktighetE'
-WHEN zoom<13 THEN 'viktighetD'
-WHEN zoom<14 THEN 'viktighetC'
-WHEN zoom<15.5 THEN 'viktighetB'
-            ELSE 'viktighetA'
-       END;
+	SELECT CASE 
+		WHEN zoom<3 THEN 'viktighetL'
+		WHEN zoom<4.8 THEN 'viktighetK'
+		WHEN zoom<7 THEN 'viktighetJ'
+		WHEN zoom<8 THEN 'viktighetI'
+		WHEN zoom<9 THEN 'viktighetH'
+		WHEN zoom<10 THEN 'viktighetG'
+		WHEN zoom<11 THEN 'viktighetF'
+		WHEN zoom<12 THEN 'viktighetE'
+		WHEN zoom<13 THEN 'viktighetD'
+		WHEN zoom<14 THEN 'viktighetC'
+		WHEN zoom<15.5 THEN 'viktighetB'
+	ELSE 'viktighetA'
+    END;
 $$ LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION stedsnavn(lng numeric, lat numeric, zoom numeric)
