5de57d54116fb3150a62ffc1560160fcd0262f33
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-06-02 10:31:12 +0200

    Cleanup

--- wms-assistent/src/App.js
+++ wms-assistent/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
-import Container from "./Container";
 import { homepage } from "../package.json";
 import TjenesteContainer from "./TjenesteContainer";
 import AppBarn from "./AppBarn";
@@ -12,19 +11,17 @@ const basename = process.env.NODE_ENV === "development" ? undefined : homepage;
 
 function App() {
   return (
-    <Container>
-      <BrowserRouter basename={basename}>
-        <CssBaseline />
-        {false && <AppBarn />}
-        <Suspense fallback="loading...">
-          <AuthenticationContextProvider>
-            <div>
-              <TjenesteContainer />
-            </div>
-          </AuthenticationContextProvider>
-        </Suspense>
-      </BrowserRouter>
-    </Container>
+    <BrowserRouter basename={basename}>
+      <CssBaseline />
+      {false && <AppBarn />}
+      <Suspense fallback="loading...">
+        <AuthenticationContextProvider>
+          <div>
+            <TjenesteContainer />
+          </div>
+        </AuthenticationContextProvider>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
