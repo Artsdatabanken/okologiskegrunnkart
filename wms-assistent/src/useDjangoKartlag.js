40056fbd93040ee8a5c6b21b34f81ae98c261fba
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-06-02 13:53:02 +0200

    Use relative path

--- wms-assistent/src/useDjangoKartlag.js
+++ wms-assistent/src/useDjangoKartlag.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 // Gjør automatisk oppdatering av Django via Form
+const url = `/admin/forvaltningsportal/wmshelper/${doc._id}/change/`;
 
 async function requestDjangoFormToken(layerNum) {
-  const url = `https://forvaltningsportaladmin.artsdatabanken.no/admin/forvaltningsportal/kartlag/${layerNum}/change/`;
   const resp = await fetch(url);
   const html = await resp.text();
   const csrfmiddlewaretoken = html.match(
@@ -20,28 +20,25 @@ async function update(doc) {
   console.log("doocc", doc);
   const csrfmiddlewaretoken = await requestDjangoFormToken(doc._id);
   console.log("csrfmiddlewaretoken", csrfmiddlewaretoken);
-  const resp = await fetch(
-    `/admin/forvaltningsportal/wmshelper/${doc._id}/change/`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: `csrfmiddlewaretoken=${csrfmiddlewaretoken}&tittel=${
-        doc.tittel
-      }&wmsurl=${encode(doc.wmsurl)}&wmsversion=${encode(
-        doc.wmsversion
-      )}&projeksjon=${encode(doc.projeksjon)}&wmsinfolayers=${encode(
-        doc.wmsinfolayers
-      )}&testkoordinater=${encode(doc.testkoordinater)}&wmsinfoformat=${encode(
-        doc.wmsinfoformat
-      )}&klikkurl=${encode(doc.klikkurl)}&klikktekst=${encode(
-        doc.klikktekst
-      )}&klikktekst2=${encode(
-        doc.klikktekst2
-      )}&_continue=Save+and+continue+editing`,
-    }
-  );
+  const resp = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `csrfmiddlewaretoken=${csrfmiddlewaretoken}&tittel=${
+      doc.tittel
+    }&wmsurl=${encode(doc.wmsurl)}&wmsversion=${encode(
+      doc.wmsversion
+    )}&projeksjon=${encode(doc.projeksjon)}&wmsinfolayers=${encode(
+      doc.wmsinfolayers
+    )}&testkoordinater=${encode(doc.testkoordinater)}&wmsinfoformat=${encode(
+      doc.wmsinfoformat
+    )}&klikkurl=${encode(doc.klikkurl)}&klikktekst=${encode(
+      doc.klikktekst
+    )}&klikktekst2=${encode(
+      doc.klikktekst2
+    )}&_continue=Save+and+continue+editing`,
+  });
 }
 
 function useDjangoKartlag() {
