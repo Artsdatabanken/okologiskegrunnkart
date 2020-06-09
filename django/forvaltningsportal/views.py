7ca70747f1bf5fbaecdaffa89e11d750757db6d9
Pedro Ramirez <pedro.ramirez@webstep.com>
2020-06-09 20:29:23 +0200

    Fixed some issues with script and added more data fields

--- django/forvaltningsportal/views.py
+++ django/forvaltningsportal/views.py
@@ -24,7 +24,7 @@ class KartlagAPIView(APIView):
             if response.ok:
                 tree = ET.ElementTree(ET.fromstring(response.content))
                 root = tree.getroot()
-                return root, tree
+                return root
             else:
                 return None
         except Exception:
@@ -32,108 +32,117 @@ class KartlagAPIView(APIView):
 
     def get(self, request: Request, *args, **kwargs):
         # url = 'http://geo.ngu.no/mapserver/MarinGrenseWMS3?REQUEST=GetCapabilities&SERVICE=WMS'
-        url = 'https://wms.nibio.no/cgi-bin/skogbruksplan?request=GetCapabilities&service=WMS&version=1.1.0'
+        # url = 'https://wms.nibio.no/cgi-bin/skogbruksplan?request=GetCapabilities&service=WMS&version=1.1.0'
         # url = 'http://geo.ngu.no/mapserver/LosmasserWMS?language=nor&srs=EPSG:900913&request=GetCapabilities&service=WMS'
         # url = 'https://kart.ra.no/arcgis/services/Distribusjon/Kulturminner20180301/MapServer/WMSServer?request=GetCapabilities&service=WMS'
         # url = 'https://wms.nibio.no/cgi-bin/ar5?request=GetCapabilities&service=WMS'
 
         all_kartlag = Kartlag.objects.all()
-        kartlag = Kartlag.objects.get(wmsurl=url)
-        print(kartlag)
+        for kartlag in all_kartlag:
+            url = kartlag.wmsurl
+            if url is None:
+                continue
+            # kartlag = Kartlag.objects.get(wmsurl=url)
+            print(kartlag)
+            print(url)
 
-        root, tree = self.get_xml_data(url)
+            root = self.get_xml_data(url)
+            if root is None:
+                continue
 
-        # Find out if tags start with {http://www.opengis.net/wms}
-        for layer in root.iter('*'):
-            if layer.tag.startswith('{http://www.opengis.net/wms}'):
-                extrainfo = '{http://www.opengis.net/wms}'
+            # Find out if tags start with {http://www.opengis.net/wms}
+            for layer in root.iter('*'):
+                if layer.tag.startswith('{http://www.opengis.net/wms}'):
+                    extrainfo = '{http://www.opengis.net/wms}'
+                else:
+                    extrainfo = ''
+                break
+
+            # Define relevant variables
+            projection_list = ['EPSG:3857', 'EPSG:900913']
+            if kartlag.projeksjon not in projection_list:
+                projection = None
             else:
-                extrainfo = ''
-            break
+                projection = kartlag.projeksjon
 
-        # Define relevant variables
-        projection_list = ['EPSG:3857', 'EPSG:900913']
-        if kartlag.projeksjon not in projection_list:
-            projection = None
-        else:
-            projection = kartlag.projeksjon
-
-        format_list = [
-		 "application/vnd.ogc.gml",
-		 "application/vnd.ogc.wms_xml",
-		 "text/xml",
-		 "application/vnd.esri.wms_featureinfo_xml",
-		 "application/vnd.esri.wms_raw_xml",
-		 "application/geojson",
-		]
-        if kartlag.wmsinfoformat not in format_list:
-            infoformat = None
-        else:
-            infoformat = kartlag.wmsinfoformat
-        
-        # Loop over all elements
-        for item in root.iter('*'):
-            # Find layers
-            if item.tag == '{}Layer'.format(extrainfo):
-                queryable_str = item.get('queryable')
-                name = item.find('{}Name'.format(extrainfo))
-                title = item.find('{}Title'.format(extrainfo))
-                if name is not None and title is not None:
-                    title = title.text
-                    name = name.text
-                    if queryable_str == '1':
-                        queryable = True
-                    else:
-                        queryable = False
-                    # print(title)
-                    print('------------')
-                    print(name)
-                    # print(queryable)
-                    if Sublag.objects.filter(Q(hovedkartlag=kartlag) & Q(wmslayer=name)).exists():
-                        print('Coming here')
-                        sublag = Sublag.objects.get(Q(hovedkartlag=kartlag) & Q(wmslayer=name))
-                        print(sublag)
-                        sublag.queryable = queryable
-                        sublag.save()
-                    else:
-                        Sublag.objects.create(
-                            wmslayer=name,
-                            tittel=title,
-                            legendeurl='',
-                            hovedkartlag=kartlag,
-                            queryable=queryable
-                        )
-            # Find wms version
-            if (item.tag == '{}WMS_Capabilities'.format(extrainfo)
-                or item.tag == '{}WMT_MS_Capabilities'.format(extrainfo)):
-                version = item.get('version')
-                if version is not None:
-                    kartlag.wmsversion = version
-                    kartlag.save()
-
-            # Find projection
-            if projection is None:
-                all_projections = item.text
-                for proj in projection_list:
-                    if all_projections is not None and proj in all_projections:
-                        projection = proj
-                        kartlag.projeksjon = proj
+            format_list = [
+            "application/vnd.ogc.gml",
+            "application/vnd.ogc.wms_xml",
+            "text/xml",
+            "application/vnd.esri.wms_featureinfo_xml",
+            "application/vnd.esri.wms_raw_xml",
+            "application/geojson",
+            ]
+            if kartlag.wmsinfoformat not in format_list:
+                infoformat = None
+            else:
+                infoformat = kartlag.wmsinfoformat
+            
+            # Loop over all elements
+            for item in root.iter('*'):
+                # Find layers
+                if item.tag == '{}Layer'.format(extrainfo):
+                    queryable_str = item.get('queryable')
+                    name = item.find('{}Name'.format(extrainfo))
+                    title = item.find('{}Title'.format(extrainfo))
+                    if name is not None and title is not None:
+                        title = title.text
+                        name = name.text
+                        if queryable_str == '1':
+                            queryable = True
+                        else:
+                            queryable = False
+                        # print(title)
+                        print('------------')
+                        print(name)
+                        # print(queryable)
+                        if Sublag.objects.filter(Q(hovedkartlag=kartlag) & Q(wmslayer=name)).exists():
+                            queryset = Sublag.objects.filter(Q(hovedkartlag=kartlag) & Q(wmslayer=name))
+                            if queryset.count() > 1:
+                                for index, lag in enumerate(queryset):
+                                    if index > 0:
+                                        lag.delete()
+                                
+                            sublag = Sublag.objects.get(Q(hovedkartlag=kartlag) & Q(wmslayer=name))
+                            sublag.queryable = queryable
+                            sublag.save()
+                            
+                        else:
+                            Sublag.objects.create(
+                                wmslayer=name,
+                                tittel=title,
+                                legendeurl='',
+                                hovedkartlag=kartlag,
+                                queryable=queryable
+                            )
+                # Find wms version
+                if (item.tag == '{}WMS_Capabilities'.format(extrainfo)
+                    or item.tag == '{}WMT_MS_Capabilities'.format(extrainfo)):
+                    version = item.get('version')
+                    if version is not None:
+                        kartlag.wmsversion = version
                         kartlag.save()
-                        break
+
+                # Find projection
+                if projection is None:
+                    all_projections = item.text
+                    for proj in projection_list:
+                        if all_projections is not None and proj in all_projections:
+                            projection = proj
+                            kartlag.projeksjon = proj
+                            kartlag.save()
+                            break
 
 
-            # Find format
-            if infoformat is None and item.tag == '{}Format'.format(extrainfo):
-                all_formats = item.text
-                for form in format_list:
-                    if all_formats is not None and form in all_formats:
-                        infoformat = form
-                        kartlag.wmsinfoformat = form
-                        kartlag.save()
-                        break
-
-
-
+                # Find format
+                if infoformat is None and item.tag == '{}Format'.format(extrainfo):
+                    all_formats = item.text
+                    for form in format_list:
+                        if all_formats is not None and form in all_formats:
+                            infoformat = form
+                            kartlag.wmsinfoformat = form
+                            kartlag.save()
+                            break
 
 
         return Response(status=status.HTTP_200_OK)
