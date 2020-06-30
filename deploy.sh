511307745e5d33a6ed6e4ad7fb7a70453e587737
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-06-30 13:56:31 +0200

    shorter

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 #Please add variables as environmental variables in Travis settings for the project when using this script.
 #These lines should also be added in the "script block" of travis.yml.
 #- export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 #- echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
-set -e
 BRANCH=$1
 PUBLIC_URL=$2
 PULL_REQUEST=${PUBLIC_URL:1}
