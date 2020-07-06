01238746327bf53d74be52acce44247390f3fe6d
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-07-06 09:01:00 +0200

    No more need to disable layers  based on zoom

--- src/Forvaltningsportalen/ForvaltningsKartlag/ForvaltningsUnderElement.js
+++ src/Forvaltningsportalen/ForvaltningsKartlag/ForvaltningsUnderElement.js
@@ -14,8 +14,6 @@ import {
   ListItemText
 } from "@material-ui/core";
 import CustomSwitch from "../../Common/CustomSwitch";
-import { zoomRangeSublayer } from "../../Funksjoner/zoomRange";
-import DisabledTooltip from "../../Common/DisabledTooltip";
 import CustomIcon from "../../Common/CustomIcon";
 
 const ForvaltningsUnderElement = ({
@@ -23,28 +21,18 @@ const ForvaltningsUnderElement = ({
   onUpdateLayerProp,
   kartlag_key,
   kartlag_owner_key,
-  valgt,
-  zoom
+  valgt
 }) => {
   let tittel = kartlag.tittel;
   const erSynlig = kartlag.erSynlig;
-  const minScaleDenominator = kartlag.minscaledenominator;
-  const maxScaleDenominator = kartlag.maxscaledenominator;
   let startstate = valgt || false;
   const [open, setOpen] = useState(startstate);
   const [sliderValue, setSliderValue] = useState(kartlag.opacity || 80);
   let kode = "underlag." + kartlag_key + ".";
 
-  const { disabled, description } = zoomRangeSublayer(
-    zoom,
-    minScaleDenominator,
-    maxScaleDenominator
-  );
-
   const handleSliderChange = value => {
     setSliderValue(value / 100);
   };
-
   const changeLayerOpacity = value => {
     setSliderValue(value / 100);
     onUpdateLayerProp(kartlag_owner_key, kode + "opacity", value / 100.0);
@@ -87,13 +75,9 @@ const ForvaltningsUnderElement = ({
                 e.stopPropagation();
               }
             }}
-            disabled={false}
           />
         </ListItemIcon>
-        <ListItemText
-          className={disabled ? "sublayer-disabled" : ""}
-          primary={tittel.nb || tittel}
-        />
+        <ListItemText primary={tittel.nb || tittel} />
         {kartlag.suggested && (
           <ListItemIcon id="bookmark-icon">
             <CustomIcon
@@ -101,7 +85,7 @@ const ForvaltningsUnderElement = ({
               icon="check-decagram"
               size={20}
               padding={0}
-              color={disabled ? "#ccc" : erSynlig ? "#666" : "#999"}
+              color={erSynlig ? "#666" : "#999"}
             />
           </ListItemIcon>
         )}
@@ -113,18 +97,8 @@ const ForvaltningsUnderElement = ({
   if (!tittel) return null;
   return (
     <>
-      {disabled ? (
-        <DisabledTooltip
-          id="tooltip-zoom-sublayer"
-          placement="left"
-          title={description}
-        >
-          <ListRow />
-        </DisabledTooltip>
-      ) : (
-        <ListRow />
-      )}
-
+      {" "}
+      <ListRow />
       <Collapse
         className="underlag_collapse"
         in={open}
@@ -151,13 +125,13 @@ const ForvaltningsUnderElement = ({
                 valueLabelDisplay="auto"
                 aria-labelledby="range-slider"
                 getAriaValueText={opacity => opacity + " %"}
-                disabled={disabled || !erSynlig}
+                disabled={!erSynlig}
               />
               <Visibility
                 id={
-                  disabled || !erSynlig
-                    ? "opacity-visible-icon-disabled"
-                    : "opacity-visible-icon"
+                  erSynlig
+                    ? "opacity-visible-icon"
+                    : "opacity-visible-icon-disabled"
                 }
                 color="primary"
                 style={{ paddingRight: 0, paddingLeft: 15 }}
