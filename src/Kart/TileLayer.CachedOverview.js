89dba93a802310a14769a299a6ca50778fbe9e02
Pedro Ramirez <pedro.ramirez@webstep.com>
2020-07-27 10:27:09 +0200

    Remove console logs

--- src/Kart/TileLayer.CachedOverview.js
+++ src/Kart/TileLayer.CachedOverview.js
@@ -2,12 +2,9 @@ import L from "leaflet";
 
 L.TileLayer.CachedOverview = L.TileLayer.WMS.extend({
   getTileUrl: function(coords) {
-    console.log(coords.z, "<", this.options.zoomThreshold);
     if (coords.z <= this.options.zoomThreshold) {
       // We should have this cached..
       var url = `https://data.test.artsdatabanken.no/grunnkart/${this.options.id}.mbtiles/${coords.z}/${coords.x}/${coords.y}`;
-      console.log({ url });
-      console.log({ dat: this });
       return url;
     }
     return L.TileLayer.WMS.prototype.getTileUrl.call(this, coords);
