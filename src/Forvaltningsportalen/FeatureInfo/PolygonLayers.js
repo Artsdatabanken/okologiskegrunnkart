aba4eca20d8b1d889bcd826eb7e6d0cb362bbb74
Pedro Ramirez <pedro.ramirez@webstep.com>
2020-09-01 10:09:59 +0200

    Improved slyte of polygon layers box

--- src/Forvaltningsportalen/FeatureInfo/PolygonLayers.js
+++ src/Forvaltningsportalen/FeatureInfo/PolygonLayers.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
-import { Button, Checkbox } from "@material-ui/core";
+import {
+  Button,
+  Checkbox,
+  ListItem,
+  ListItemText,
+  Collapse
+} from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
 import "../../style/infobox.css";
 
 const PolygonLayers = ({ polygon }) => {
-  const availableLayers = [{ name: "Kommuner og Fylker", selected: false }];
+  const availableLayers = [
+    { name: "Kommuner og Fylker", selected: false },
+    { name: "Test uten data", selected: false }
+  ];
   const [searchLayers, setSearchLayers] = useState(availableLayers);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const calculateLayers = () => {
     console.log(searchLayers);
@@ -23,32 +34,50 @@ const PolygonLayers = ({ polygon }) => {
 
   return (
     <div className="polygon-layers-wrapper">
-      <div className="polygon-layers-content">
-        {searchLayers.map((layer, index) => {
-          return (
-            <div key={index} className="polygon-layers-item">
-              <div className="polygon-layers-name">{layer.name}</div>
-              <Checkbox
-                checked={layer.selected}
-                onChange={e => handleChange(e, layer.name)}
-                color="default"
-              />
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        <Button
-          id="polygon-run-button"
-          variant="contained"
-          size="small"
-          onClick={() => {
-            calculateLayers();
-          }}
-        >
-          Hent resultater
-        </Button>
-      </div>
+      <ListItem
+        id="polygon-layer-expander"
+        button
+        divider
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <ListItemText primary="Velg lag" />
+        {menuOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={menuOpen} timeout="auto" unmountOnExit>
+        <div className="polygon-layers-content">
+          <div className="polygon-checkbox-content">
+            {searchLayers.map((layer, index) => {
+              return (
+                <div key={index} className="polygon-layers-item">
+                  <div className={polygon ? "" : "polygon-layers-disabled"}>
+                    {layer.name}
+                  </div>
+                  <Checkbox
+                    id="select-layers-checkbox"
+                    checked={layer.selected}
+                    onChange={e => handleChange(e, layer.name)}
+                    color="default"
+                    disabled={polygon ? false : true}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <div className="polygon-button-wrapper">
+            <Button
+              id="polygon-run-button"
+              variant="contained"
+              size="small"
+              onClick={() => {
+                calculateLayers();
+              }}
+              disabled={polygon ? false : true}
+            >
+              Hent resultater
+            </Button>
+          </div>
+        </div>
+      </Collapse>
     </div>
   );
 };
