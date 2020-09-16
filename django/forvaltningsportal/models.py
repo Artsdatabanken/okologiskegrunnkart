932722aca760372021f649aa5f642f29bad3374c
Pedro Ramirez <pedro.ramirez@webstep.com>
2020-09-16 21:40:44 +0200

    Change in Django. Do not include in kartlag.json a layer if no sublayers are published

--- django/forvaltningsportal/models.py
+++ django/forvaltningsportal/models.py
@@ -114,15 +114,18 @@ def createJSON(sender, instance, **kwargs):
 
     dict = {}
     for kartlag in Kartlag.objects.all():
-        # legg til sjekk her for om det står publiser når vi lager egen fil til prod
-        dict[kartlag.id] = {
-            'id': kartlag.id,
-            'dataeier': kartlag.dataeier.tittel,
-            'tittel': kartlag.tittel
-        }
+        # Check if layer contains at least one published sublayer
+        number_underlag = kartlag.sublag.filter(publiser=True).count()
+        if number_underlag > 0:
+            dict[kartlag.id] = {
+                'id': kartlag.id,
+                'dataeier': kartlag.dataeier.tittel,
+                'tittel': kartlag.tittel
+            }
 
-        if kartlag.sublag.count():
-            alle_underlag = kartlag.sublag.all()
+        # if kartlag.sublag.count():
+            # alle_underlag = kartlag.sublag.all()
+            alle_underlag = kartlag.sublag.filter(publiser=True)
             underlag = {}
             for lag in alle_underlag:
                 if lag.publiser:
@@ -188,44 +191,44 @@ def createJSON(sender, instance, **kwargs):
             
             dict[kartlag.id]['underlag'] = underlag
 
-        if kartlag.dataeier.logourl:
-            dict[kartlag.id]['logourl'] = kartlag.dataeier.logourl
-        if kartlag.dataeier.url:
-            dict[kartlag.id]['kildeurl'] = kartlag.dataeier.url
-        if kartlag.type:
-            dict[kartlag.id]['type'] = kartlag.type.tittel
-        if kartlag.tema:
-            dict[kartlag.id]['tema'] = kartlag.tema.tittel
-        if kartlag.faktaark:
-            dict[kartlag.id]['faktaark'] = kartlag.faktaark
-        if kartlag.produktark:
-            dict[kartlag.id]['produktark'] = kartlag.produktark
-        if kartlag.geonorgeurl:
-            dict[kartlag.id]['geonorgeurl'] = kartlag.geonorgeurl
-        if kartlag.wmsurl:
-            dict[kartlag.id]['wmsurl'] = kartlag.wmsurl
-        if kartlag.wmsversion:
-            dict[kartlag.id]['wmsversion'] = kartlag.wmsversion
-        if kartlag.projeksjon:
-            dict[kartlag.id]['projeksjon'] = kartlag.projeksjon
-        if kartlag.wmsinfoformat:
-            dict[kartlag.id]['wmsinfoformat'] = kartlag.wmsinfoformat
-        if kartlag.testkoordinater:
-            dict[kartlag.id]['testkoordinater'] = kartlag.testkoordinater
-        if kartlag.klikkurl:
-            dict[kartlag.id]['klikkurl'] = kartlag.klikkurl
-        if kartlag.klikktekst:
-            dict[kartlag.id]['klikktekst'] = kartlag.klikktekst
-        if kartlag.klikktekst:
-            dict[kartlag.id]['klikktekst2'] = kartlag.klikktekst2
-        if kartlag.aggregatedwmslayer:
-            dict[kartlag.id]['aggregatedwmslayer'] = kartlag.aggregatedwmslayer
+            if kartlag.dataeier.logourl:
+                dict[kartlag.id]['logourl'] = kartlag.dataeier.logourl
+            if kartlag.dataeier.url:
+                dict[kartlag.id]['kildeurl'] = kartlag.dataeier.url
+            if kartlag.type:
+                dict[kartlag.id]['type'] = kartlag.type.tittel
+            if kartlag.tema:
+                dict[kartlag.id]['tema'] = kartlag.tema.tittel
+            if kartlag.faktaark:
+                dict[kartlag.id]['faktaark'] = kartlag.faktaark
+            if kartlag.produktark:
+                dict[kartlag.id]['produktark'] = kartlag.produktark
+            if kartlag.geonorgeurl:
+                dict[kartlag.id]['geonorgeurl'] = kartlag.geonorgeurl
+            if kartlag.wmsurl:
+                dict[kartlag.id]['wmsurl'] = kartlag.wmsurl
+            if kartlag.wmsversion:
+                dict[kartlag.id]['wmsversion'] = kartlag.wmsversion
+            if kartlag.projeksjon:
+                dict[kartlag.id]['projeksjon'] = kartlag.projeksjon
+            if kartlag.wmsinfoformat:
+                dict[kartlag.id]['wmsinfoformat'] = kartlag.wmsinfoformat
+            if kartlag.testkoordinater:
+                dict[kartlag.id]['testkoordinater'] = kartlag.testkoordinater
+            if kartlag.klikkurl:
+                dict[kartlag.id]['klikkurl'] = kartlag.klikkurl
+            if kartlag.klikktekst:
+                dict[kartlag.id]['klikktekst'] = kartlag.klikktekst
+            if kartlag.klikktekst:
+                dict[kartlag.id]['klikktekst2'] = kartlag.klikktekst2
+            if kartlag.aggregatedwmslayer:
+                dict[kartlag.id]['aggregatedwmslayer'] = kartlag.aggregatedwmslayer
 
-        if kartlag.tag:
-            list = []
-            for tag in kartlag.tag.all():
-                list.append(tag.tittel)
-            dict[kartlag.id]['tags'] = list
+            if kartlag.tag:
+                list = []
+                for tag in kartlag.tag.all():
+                    list.append(tag.tittel)
+                dict[kartlag.id]['tags'] = list
 
         # kartlag-fil for bruk i wms-hjelpeverktøy
         # Når vi setter opp den automatiske koblingen mellom WMS-admin-toolen og Django, legg til denne igjen
