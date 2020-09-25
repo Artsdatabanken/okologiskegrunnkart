320971351b7559ef356478a390043441f859309b
Pedro Ramirez <pedro.ramirez@webstep.com>
2020-09-25 14:58:26 +0200

    Sort and filter is only recalculated when changed, not for every render

--- src/Forvaltningsportalen/ForvaltningsKartlag/ForvaltningsKartlag.js
+++ src/Forvaltningsportalen/ForvaltningsKartlag/ForvaltningsKartlag.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import ForvaltningsGruppering from "./ForvaltningsGruppering";
 import { Chip, Typography, List, Button } from "@material-ui/core";
 import Sortering from "./Sortering";
@@ -19,9 +19,12 @@ const ForvaltningsKartlag = ({
   const [hideHidden] = useState(false); // Not used??? Never changes...
   const [searchTerm] = useState(null); // Not used??? Never changes...
   const [matchAllFilters, setMatchAllFilters] = useState(true);
+  const [sorted, setSorted] = useState({});
+  const [taglist, setTaglist] = useState([]);
+  const [tags, setTags] = useState(null);
 
   const handleFilterTag = (tag, value) => {
-    let tagFilterTemp = tagFilter;
+    let tagFilterTemp = { ...tagFilter };
     tagFilterTemp[tag] = value;
     setTagFilter(tagFilterTemp);
   };
@@ -36,48 +39,62 @@ const ForvaltningsKartlag = ({
     dataeier: "Gruppert på dataeier"
   };
 
-  let lag = kartlag;
-  let sorted = {};
+  useEffect(() => {
+    let lag = kartlag;
+    let sorted = {};
 
-  // Henter ut unike tags for checklistegenerering
-  let taglist = {};
-  for (let i in lag) {
-    for (let tag of lag[i]["tags"] || []) taglist[tag] = true;
-  }
-
-  taglist = Object.keys(taglist).sort();
-
-  // Sorterer listen på valgt kriterie
-  console.log("Render");
-  for (let item in lag) {
-    console.log("Sort results");
-    let criteria = lag[item][sortKey];
-    let new_list = [];
-    if (!criteria) {
-      criteria = "";
+    // Henter ut unike tags for checklistegenerering
+    let taglist = {};
+    for (let i in lag) {
+      for (let tag of lag[i]["tags"] || []) {
+        taglist[tag] = true;
+      }
     }
-    if (sorted[criteria]) {
-      new_list = sorted[criteria];
-    }
-    lag[item].id = item;
-    new_list.push(lag[item]);
-    sorted[criteria] = new_list;
-  }
+    taglist = Object.keys(taglist).sort();
+    setTaglist(taglist);
 
-  const tags = taglist
-    .filter(tag => tagFilter[tag])
-    .map(tag => (
-      <Chip
-        id="layers-filter-chips"
-        style={{ margin: "2px 5px 2px 0" }}
-        key={tag}
-        label={tag}
-        clickable
-        color="primary"
-        onClick={() => handleFilterTag(tag, !tagFilter[tag])}
-        onDelete={() => handleFilterTag(tag, false)}
-      />
-    ));
+    // Sorterer listen på valgt kriterie
+    for (let item in lag) {
+      let criteria = lag[item][sortKey];
+      let new_list = [];
+      if (!criteria) {
+        criteria = "";
+      }
+      if (sorted[criteria]) {
+        new_list = sorted[criteria];
+      }
+      lag[item].id = item;
+      new_list.push(lag[item]);
+      sorted[criteria] = new_list;
+    }
+    setSorted(sorted);
+  }, [kartlag, sortKey]);
+
+  useEffect(() => {
+    const tags = taglist
+      .filter(tag => tagFilter[tag])
+      .map(tag => (
+        <Chip
+          id="layers-filter-chips"
+          style={{ margin: "2px 5px 2px 0" }}
+          key={tag}
+          label={tag}
+          clickable
+          color="primary"
+          onClick={() => {
+            let tagFilterTemp = { ...tagFilter };
+            tagFilterTemp[tag] = !tagFilter[tag];
+            setTagFilter(tagFilterTemp);
+          }}
+          onDelete={() => {
+            let tagFilterTemp = { ...tagFilter };
+            tagFilterTemp[tag] = false;
+            setTagFilter(tagFilterTemp);
+          }}
+        />
+      ));
+    setTags(tags);
+  }, [taglist, tagFilter]);
 
   return (
     <>
@@ -170,170 +187,5 @@ const ForvaltningsKartlag = ({
     </>
   );
 };
-
-// class ForvaltningsKartlag extends React.Component {
-//   // Denne funksjonen tar inn alle lagene som sendes inn, og henter ut per eier
-//   state = {
-//     sortKey: "alfabetisk",
-//     tagFilter: {},
-//     hideHidden: false,
-//     searchTerm: null,
-//     showFilter: false,
-//     matchAllFilters: true
-//   };
-
-//   handleFilterTag = (tag, value) => {
-//     let tagFilter = this.state.tagFilter;
-//     tagFilter[tag] = value;
-//     this.setState({ tagFilter });
-//   };
-
-//   handleChangeSort = sortKey => {
-//     this.setState({ sortKey });
-//   };
-
-//   sortKeyToDescription = {
-//     alfabetisk: "Alfabetisk",
-//     tema: "Gruppert på tema",
-//     dataeier: "Gruppert på dataeier"
-//   };
-
-//   render() {
-//     const { toggleSublayer, toggleAllSublayers } = this.props;
-//     let lag = this.props.kartlag;
-//     let sortKey = this.state.sortKey;
-//     let sorted = {};
-
-//     // Henter ut unike tags for checklistegenerering
-//     let taglist = {};
-//     for (let i in lag)
-//       for (let tag of lag[i]["tags"] || []) taglist[tag] = true;
-
-//     taglist = Object.keys(taglist).sort();
-
-//     // Sorterer listen på valgt kriterie
-//     console.log("Render")
-//     for (let item in lag) {
-//       console.log("Sort results")
-//       let criteria = lag[item][sortKey];
-//       let new_list = [];
-//       if (!criteria) {
-//         criteria = "";
-//       }
-//       if (sorted[criteria]) {
-//         new_list = sorted[criteria];
-//       }
-//       lag[item].id = item;
-//       new_list.push(lag[item]);
-//       sorted[criteria] = new_list;
-//     }
-
-//     const tags = taglist
-//       .filter(tag => this.state.tagFilter[tag])
-//       .map(tag => (
-//         <Chip
-//           id="layers-filter-chips"
-//           style={{ margin: "2px 5px 2px 0" }}
-//           key={tag}
-//           label={tag}
-//           clickable
-//           color="primary"
-//           onClick={() => this.handleFilterTag(tag, !this.state.tagFilter[tag])}
-//           onDelete={() => this.handleFilterTag(tag, false)}
-//         />
-//       ));
-
-//     return (
-//       <>
-//         <div className="header-layers-menu">
-//           <div className="sort-filter-layers-wrapper">
-//             <div>
-//               <Typography variant="h6">Kartlag</Typography>
-//               <Typography variant="body2">
-//                 {this.sortKeyToDescription[sortKey]}
-//               </Typography>
-//             </div>
-//             <div className="sort-filter-icons-wrapper">
-//               <Sortering sort={sortKey} onChangeSort={this.handleChangeSort} />
-//               <Filtrering
-//                 taglist={taglist}
-//                 tagFilter={this.state.tagFilter}
-//                 onFilterTag={this.handleFilterTag}
-//               />
-//             </div>
-//           </div>
-//         </div>
-//         {tags && tags.length > 0 && (
-//           <div className="selected-tags-wrapper">
-//             <div className="selected-tags-tittle">
-//               <Typography id="filters-header" variant="body2">
-//                 Filtrer
-//               </Typography>
-//               {tags.length > 1 && (
-//                 <div className="filter-options-wrapper">
-//                   <Button
-//                     id="filter-all-button"
-//                     size="small"
-//                     color={this.state.matchAllFilters ? "primary" : "secondary"}
-//                     onClick={() => {
-//                       this.setState({ matchAllFilters: true });
-//                     }}
-//                   >
-//                     Matcher alle
-//                   </Button>
-//                   /
-//                   <Button
-//                     id="filter-all-button"
-//                     size="small"
-//                     color={this.state.matchAllFilters ? "secondary" : "primary"}
-//                     onClick={() => {
-//                       this.setState({ matchAllFilters: false });
-//                     }}
-//                   >
-//                     Matcher minst ett
-//                   </Button>
-//                 </div>
-//               )}
-//             </div>
-//             {tags.reduce((accu, elem, index) => {
-//               return accu === null ? [elem] : [...accu, elem];
-//             }, null)}
-//           </div>
-//         )}
-
-//         <div className="legend-link-background">
-//           <div className="legend-link-wrapper">
-//             <TegnforklaringLink
-//               layers={this.props.kartlag}
-//               setLegendVisible={this.props.setLegendVisible}
-//             />
-//           </div>
-//         </div>
-
-//         <List id="layers-list-wrapper">
-//           {Object.keys(sorted)
-//             .reverse()
-//             .map(element => {
-//               return (
-//                 <ForvaltningsGruppering
-//                   searchTerm={this.state.searchTerm}
-//                   hideHidden={this.state.hideHidden}
-//                   tagFilter={this.state.tagFilter}
-//                   matchAllFilters={this.state.matchAllFilters}
-//                   kartlag={sorted[element]}
-//                   element={element}
-//                   key={element}
-//                   toggleSublayer={toggleSublayer}
-//                   toggleAllSublayers={toggleAllSublayers}
-//                   zoom={this.props.zoom}
-//                   showSublayerDetails={this.props.showSublayerDetails}
-//                 />
-//               );
-//             })}
-//         </List>
-//       </>
-//     );
-//   }
-// }
 
 export default ForvaltningsKartlag;
