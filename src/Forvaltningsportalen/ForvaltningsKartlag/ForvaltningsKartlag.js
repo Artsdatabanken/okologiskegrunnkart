708fd95e83edfb5065157544fc2fd7118b5cd3b9
Pedro Ramirez <pedro.ramirez@webstep.com>
2020-09-25 15:05:17 +0200

    Remove unused variable

--- src/Forvaltningsportalen/ForvaltningsKartlag/ForvaltningsKartlag.js
+++ src/Forvaltningsportalen/ForvaltningsKartlag/ForvaltningsKartlag.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import ForvaltningsGruppering from "./ForvaltningsGruppering";
 import { Chip, Typography, List, Button } from "@material-ui/core";
 import Sortering from "./Sortering";
