3408ad0c3fb11d37060e20accee9d4b757fd0112
Pedro Ramirez <pedro.ramirez@webstep.com>
2020-10-26 22:28:50 +0100

    Added preliminary modal with info about the App

--- src/Forvaltningsportalen/AppName.js
+++ src/Forvaltningsportalen/AppName.js
@@ -1,20 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/appname.css";
-import { Snackbar } from "@material-ui/core";
+import { Snackbar, Button, Modal } from "@material-ui/core";
+import { Close } from "@material-ui/icons";
 
 const AppName = ({ showAppName, closeAppName }) => {
+  const [showAboutModal, setShowAboutModal] = useState(false);
+
+  const openAboutModal = () => {
+    setShowAboutModal(true);
+    console.log("Opening");
+  };
+
+  const closeAboutModal = () => {
+    setShowAboutModal(false);
+  };
+
   return (
-    <Snackbar
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "center"
-      }}
-      open={showAppName}
-      onClose={e => closeAppName()}
-      severity
-    >
-      <div className="app-name-wrapper">Økologiske grunnkart</div>
-    </Snackbar>
+    <>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+        open={showAppName}
+        onClose={() => closeAppName()}
+      >
+        <div className="app-name-wrapper">
+          <div className="app-name-text">Økologiske grunnkart</div>
+          <Button
+            id="read-more-about"
+            variant="contained"
+            size="small"
+            onClick={() => openAboutModal()}
+            color="primary"
+          >
+            Mer info
+          </Button>
+        </div>
+      </Snackbar>
+
+      <Modal
+        open={showAboutModal}
+        onClose={closeAboutModal}
+        className="help-modal-body"
+      >
+        <div className="help-modal-wrapper">
+          <div className="help-modal-title">
+            <div>Om "Økologiske Grunnkart"</div>
+            <button
+              tabIndex="0"
+              className="close-modal-button-wrapper"
+              onClick={e => {
+                closeAboutModal();
+              }}
+            >
+              <div className="close-modal-button">
+                <Close />
+              </div>
+            </button>
+          </div>
+          <div className="help-modal-content">This is a description</div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
