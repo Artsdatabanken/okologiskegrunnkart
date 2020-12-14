fb29382a64e42e3bf22e7a90f05f26f67dfba88a
Pedro Ramirez <pedro.ramirez@webstep.com>
2020-12-14 10:15:09 +0100

    Added tests for filtering of kartlag

--- tests/OkologiskeGrunnkart/GrunnKartlag/GrunnKartlag.test.js
+++ tests/OkologiskeGrunnkart/GrunnKartlag/GrunnKartlag.test.js
@@ -75,7 +75,7 @@ it("should render all layer names and buttons in kartlag sorted by tema", () => 
   getByTitle("Åpne på venstre side");
 });
 
-it("should render all layer names and buttons in kartlag sorted alphabetically", async () => {
+it("should sort kartlag alphabetically", async () => {
   const {
     getAllByText,
     getByText,
@@ -119,7 +119,7 @@ it("should render all layer names and buttons in kartlag sorted alphabetically",
   expect(tema.length).toBe(1);
 });
 
-it("should render all layer names and buttons in kartlag sorted by data owner", async () => {
+it("should sort kartlag by data owner", async () => {
   const {
     getAllByText,
     getByText,
@@ -168,4 +168,147 @@ it("should render all layer names and buttons in kartlag sorted by data owner", 
   owner = getAllByText("Artsdatabanken");
   expect(owner.length).toBe(2);
   expect(owner[0]).toHaveClass("MuiListSubheader-root");
+});
+
+it("should filter kartlag by arter", async () => {
+  const {
+    getAllByText,
+    getByText,
+    getByTitle,
+    findByText,
+    queryByText
+  } = renderGrunnKartlag();
+  getByText("Kartlag");
+  getByText("Gruppert på tema");
+
+  getByText("Livsmiljøer");
+  getByText("Arealressurs: AR5");
+  getByText("Naturtyper - NiN Mdir");
+  getByText("Naturtyper - DN Håndbok 13");
+  getByText("Arter - Rødlista");
+
+  getByText("Arealressurs");
+  getByText("Naturtyper");
+  getByText("Skog");
+  let arter = getAllByText("Arter");
+  expect(arter.length).toBe(2);
+
+  // Select filter by DOK-data and Naturtype
+  let filtrer = queryByText("Filtrer");
+  expect(filtrer).toBeNull();
+  fireEvent.click(getByTitle("Filtrer"));
+  await waitFor(() => findByText("DOK-data"));
+  fireEvent.click(arter[1]);
+
+  // Only arter rødlista is visible
+  getByText("Arter - Rødlista");
+  let layer = screen.queryByTitle("Arealressurs: AR5");
+  expect(layer).toBeNull();
+  layer = screen.queryByTitle("Livsmiljøer");
+  expect(layer).toBeNull();
+  layer = screen.queryByTitle("Naturtyper - NiN Mdir");
+  expect(layer).toBeNull();
+  layer = screen.queryByTitle("Naturtyper - DN Håndbok 13");
+  expect(layer).toBeNull();
+
+  // Filter badges are visible
+  getByText("Filtrer");
+  arter = getAllByText("Arter");
+  expect(arter.length).toBe(3);
+  expect(arter[0]).toHaveClass("MuiChip-label");
+});
+
+it("should filter kartlag by multiple terms", async () => {
+  const {
+    getAllByText,
+    getByText,
+    getByTitle,
+    findByText,
+    queryByText
+  } = renderGrunnKartlag();
+  getByText("Kartlag");
+  getByText("Gruppert på tema");
+
+  getByText("Livsmiljøer");
+  getByText("Arealressurs: AR5");
+  getByText("Naturtyper - NiN Mdir");
+  getByText("Naturtyper - DN Håndbok 13");
+  getByText("Arter - Rødlista");
+
+  getByText("Arealressurs");
+  getByText("Naturtyper");
+  getByText("Skog");
+  let arter = getAllByText("Arter");
+  expect(arter.length).toBe(2);
+
+  // Select filter by DOK-data and Naturtype
+  let filtrer = queryByText("Filtrer");
+  expect(filtrer).toBeNull();
+  fireEvent.click(getByTitle("Filtrer"));
+  await waitFor(() => findByText("Feltkartlagte data"));
+  fireEvent.click(getByText("DOK-data"));
+  await waitFor(() => findByText("Filtrer"));
+  fireEvent.click(getByTitle("Filtrer"));
+  await waitFor(() => findByText("Feltkartlagte data"));
+  fireEvent.click(getByText("Naturtype"));
+
+  // Only arealressurs and naturtyper are visible
+  getByText("Arealressurs: AR5");
+  getByText("Naturtyper - NiN Mdir");
+  getByText("Naturtyper - DN Håndbok 13");
+  let layer = screen.queryByTitle("Arter - Rødlista");
+  expect(layer).toBeNull();
+  layer = screen.queryByTitle("Livsmiljøer");
+  expect(layer).toBeNull();
+
+  // Filter badges are visible
+  let naturtype = getAllByText("Naturtype");
+  expect(naturtype.length).toBe(2);
+  expect(naturtype[0]).toHaveClass("MuiChip-label");
+  let dokdata = getAllByText("DOK-data");
+  expect(dokdata.length).toBe(2);
+  expect(dokdata[0]).toHaveClass("MuiChip-label");
+});
+
+it("should allow different matches when filtered by multiple terms", async () => {
+  const { getByText, getByTitle, findByText } = renderGrunnKartlag();
+  getByText("Kartlag");
+  getByText("Gruppert på tema");
+
+  getByText("Livsmiljøer");
+  getByText("Arealressurs: AR5");
+  getByText("Naturtyper - NiN Mdir");
+  getByText("Naturtyper - DN Håndbok 13");
+  getByText("Arter - Rødlista");
+
+  // Select filter by DOK-data and Naturtype
+  fireEvent.click(getByTitle("Filtrer"));
+  await waitFor(() => findByText("Feltkartlagte data"));
+  fireEvent.click(getByText("DOK-data"));
+  await waitFor(() => findByText("Filtrer"));
+  fireEvent.click(getByTitle("Filtrer"));
+  await waitFor(() => findByText("Feltkartlagte data"));
+  fireEvent.click(getByText("Naturtype"));
+
+  // Only arealressurs and naturtyper are visible
+  getByText("Arealressurs: AR5");
+  getByText("Naturtyper - NiN Mdir");
+  getByText("Naturtyper - DN Håndbok 13");
+  let layer = screen.queryByTitle("Arter - Rødlista");
+  expect(layer).toBeNull();
+  layer = screen.queryByTitle("Livsmiljøer");
+  expect(layer).toBeNull();
+
+  // Switch to match at least one
+  getByText("Matcher alle");
+  getByText("Matcher minst ett");
+  fireEvent.click(getByText("Matcher minst ett"));
+
+  // Now Livsmiljøer is also visible
+  getByText("Arealressurs: AR5");
+  getByText("Naturtyper - NiN Mdir");
+  getByText("Naturtyper - DN Håndbok 13");
+  getByText("Livsmiljøer");
+  layer = screen.queryByTitle("Arter - Rødlista");
+  expect(layer).toBeNull();
 });
