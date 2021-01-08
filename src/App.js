4f053ef22794a81ae8413a66e9df31d8bd6f49e7
Pedro Ramirez <pedro.ramirez@webstep.com>
2021-01-08 14:12:10 +0100

    Fix a minor bug. After editting and saving favorites, URL wasn't being changed to favorites=true

--- src/App.js
+++ src/App.js
@@ -382,7 +382,7 @@ class App extends React.Component {
     } else if ((!lat || !lng) && layers) {
       layersUrlString = "layers=" + layers;
     }
-    // Whern mounted, favorites is updated based on the real value
+    // When mounted, favorites is updated based on the real value
     // from state, since this is calulated when loading kartlag
     let favoritesUrlString;
     if ((lat && lng) || layers) {
@@ -850,7 +850,16 @@ class App extends React.Component {
   };
 
   handleShowFavoriteLayers = async showFavoriteLayers => {
+    this.handleUpdateChangeInUrl(false);
     this.setState({ showFavoriteLayers });
+
+    // Update URL
+    var queryParams = new URLSearchParams(window.location.search);
+    queryParams.set("favorites", showFavoriteLayers);
+    this.props.history.push("?" + queryParams.toString());
+    setTimeout(() => {
+      this.handleUpdateChangeInUrl(true);
+    }, 50);
   };
 
   toggleShowFavoriteLayers = async favorites => {
