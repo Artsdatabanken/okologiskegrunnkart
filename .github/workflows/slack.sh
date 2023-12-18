e44cb2f167faf3ecce674ff6c8d3050fbb199981
Bjørn Reppen <bjorn.reppen@artsdatabanken.no>
2023-12-18 10:58:01 +0100

    Deploy wms-assistent

--- .github/workflows/slack.sh
+++ .github/workflows/slack.sh
@@ -1,1 +1,2 @@
 curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy forvaltningsportal"}' $1
+curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy wms-ass"}' $1
